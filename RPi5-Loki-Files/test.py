91b06179808944e8c499e42a7a2dad6aa0ae2b62
Patrick Polanco <patapolpersonal@gmail.com>
2024-03-29 22:07:13 -0700

    Adding code from Chat-GPT to test.py for RPi Loki. This will be used to learn GPIOD.

--- RPi5-Loki-Files/test.py
+++ RPi5-Loki-Files/test.py
@@ -1,10 +1,28 @@
-import gpiozero as gz
+# Import necessary modules
+import gpiod
 import time
 
-print("Hello There")
+# Define GPIO chip and line numbers
+CHIP = "gpiochip0"  # Change to the appropriate chip name
+LINE_NUM = 17       # GPIO 17
 
-status, count = 0
-led = gz.LED(16)
+# Open the GPIO chip
+chip = gpiod.Chip(CHIP)
 
-while(count < 10):
-    led.toggle()
+# Get the GPIO line
+line = chip.get_line(LINE_NUM)
+
+# Request the line for output
+line.request(consumer="my-led", type=gpiod.LINE_REQ_DIR_OUT)
+
+try:
+    while True:
+        # Toggle the LED
+        line.set_value(1)
+        time.sleep(1)
+        line.set_value(0)
+        time.sleep(1)
+except KeyboardInterrupt:
+    # Clean up
+    line.release()
+    chip.close()
