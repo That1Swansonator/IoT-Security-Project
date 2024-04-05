82e1e838b9a04ee0b86bb27d6063fa4e80c62308
patrick <patapoldev@gmail.com>
2024-04-05 16:22:23 -0700

    Redid code to open and close at the end of an enclosed statement

--- RPi5-Loki-Files/test.py
+++ RPi5-Loki-Files/test.py
@@ -4,31 +4,29 @@ import time
 
 # Define GPIO chip and line numbers
 CHIP = "/dev/gpiochip0"  # Change to the appropriate chip name
-LINE_NUM = 17       # GPIO 17
+LINE_NUM = 17  # GPIO 17
 
 # Open the GPIO chip
-chip = gpiod.Chip(CHIP)
+# chip = gpiod.Chip(CHIP)
 
-# Get the GPIO line
-line = gpiod.Line(chip, LINE_NUM)
+with gpiod.Chip(CHIP) as chip:
+    # The chip is automatically opened here & You can use the chip within this context
+    led = chip.get_line(LINE_NUM)
 
-# request the lines 17 and 39 using the request_lines method
-#lines = chip.request_lines([LINE_NUM, 39], consumer="my-led", type=gpiod.LINE_REQ_DIR_OUT)
+    config = gpiod.line_request
+    config.consumer = "Blink"
+    config.request_type = gpiod.line_request.DIRECTION_OUTPUT
+    #config.type = gpiod.LINE_REQ_DIR_OUT
 
+    led.request(config)
 
+    print(led.consumer)
 
-
-# Request the line for output
-#line.request(consumer="my-led", type=gpiod.LINE_REQ_DIR_OUT)
-
-try:
     while True:
-        # Toggle the LED
-        line.set_value(1)
-        time.sleep(1)
-        line.set_value(0)
-        time.sleep(1)
-except KeyboardInterrupt:
-    # Clean up
-    line.release()
-    chip.close()
+        led.set_value(0)
+        time.sleep(0.1)
+        led.set_value(1)
+        time.sleep(0.1)
+
+    pass
+
