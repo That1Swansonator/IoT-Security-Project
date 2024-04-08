5d0d81eedd4b54ab36253c094f9ae9874cf117f0
patrick <patapoldev@gmail.com>
2024-04-08 16:24:51 -0700

    Testing for pins

--- RPi5-Loki-Files/test.py
+++ RPi5-Loki-Files/test.py
@@ -3,7 +3,7 @@ from time import sleep
 import gpiozero
 
 # Create a new LED object from the GPIO Zero library and assign it to the variable led at pin 2 and a ground pin
-led = gpiozero.LED(2)
+led = gpiozero.LED(11)
 
 # blink the LED on and off every 1 second
 for i in range(10):
