537da55e4353b6b009d5bf3a4fd3560debf5baf7
patrick <patapoldev@gmail.com>
2024-04-08 16:46:36 -0700

    Changing to bcm IDing

--- RPi5-Loki-Files/test.py
+++ RPi5-Loki-Files/test.py
@@ -2,8 +2,8 @@ from time import sleep
 
 import gpiozero
 
-# Create a new LED object from the GPIO Zero library and assign it to the variable led at gpio pin 17
-led = gpiozero.LED(13)
+# Create a new LED object from the GPIO Zero library and assign it to the variable led at pin 17
+led = gpiozero.LED(17)
 
 # blink the LED on and off every 1 second
 for i in range(10):
