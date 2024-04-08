48a15d26582d9dc08a88d7b4b6ba5f1965dbbafb
patrick <patapoldev@gmail.com>
2024-04-08 16:12:30 -0700

    Retrying gpiozero

--- RPi5-Loki-Files/test.py
+++ RPi5-Loki-Files/test.py
@@ -1,3 +1,14 @@
-from example import function
+from time import sleep
 
-print(function(1, 2))
+import gpiozero
+
+# Create a new LED object from the GPIO Zero library and assign it to the variable led at pin 2 and a ground pin
+led = gpiozero.LED(2)
+
+# blink the LED on and off every 1 second
+for i in range(10):
+    led.blink(on_time=1, off_time=1)
+    sleep(5)
+
+
+
