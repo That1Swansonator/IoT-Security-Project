2c54a4a4602835ad2733090ea48818626e3fa09b
patrick <patapoldev@gmail.com>
2024-04-08 16:57:50 -0700

    Debugging non-blinking led issues.

--- RPi5-Loki-Files/test.py
+++ RPi5-Loki-Files/test.py
@@ -1,14 +1,15 @@
 from time import sleep
-
 import gpiozero
 
-# Create a new LED object from the GPIO Zero library and assign it to the variable led at pin 17
-led = gpiozero.LED(17)
+try:
+    # Create a new LED object from the GPIO Zero library and assign it to the variable led at pin 17
+    led = gpiozero.LED(17)
 
-# blink the LED on and off every 1 second
-for i in range(10):
-    led.blink(on_time=1, off_time=1)
-    sleep(5)
-
+    # blink the LED on and off every 1 second
+    for i in range(10):
+        led.blink(on_time=1, off_time=1)
+        sleep(5)
+except Exception as e:
+    print(f"An error occurred: {e}")
 
 
