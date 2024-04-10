dc14efa809585ea43783fe8215de4a64b90cf632
patrick <patapoldev@gmail.com>
2024-04-09 22:19:37 -0700

    Testing some copilot suggestions that should blink an LED at pin 17.

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 My primary goal is to create a secure and bespoke IoT network based on the three-layer approach using three security methods.
 
 A bash file will; be made as a sort of installer.
-For now, python setup.py build_ext --inplace is the command to get the needed .so files for  cython
 rm /usr/lib/python3.11/EXTERNALLY-MANAGED for removing the PIP installation roadblock
+
+The following bash commands is to install the necessary packages to control the gpio pins on the Raspberry Pi.
+wget https://github.com/joan2937/lg/archive/master.zip
+unzip master.zip
+cd lg-master
+make
+sudo make install
+
+
--- RPi5-Loki-Files/test.c
+++ RPi5-Loki-Files/test.c
@@ -4,8 +4,32 @@
 
 #include "stdio.h"
 #include "stdlib.h"
+#include "gpiod.h"
 
 int main(void){
+    // select the chip gpiochip0
+    struct gpiod_chip *chip = gpiod_chip_open("/dev/gpiochip0");
+
+    if (!chip) {
+        perror("Open chip failed\n");
+        return EXIT_FAILURE;
+    }
+
+    // get the line 17
+    struct gpiod_line *line = gpiod_chip_get_line(chip, 17);
+
+    if (!line) {
+        perror("Get line failed\n");
+        return EXIT_FAILURE;
+    }
+
+    // blink the led
+    for (int i = 0; i < 10; i++) {
+        gpiod_line_set_value(line, 1);
+        sleep(1);
+        gpiod_line_set_value(line, 0);
+        sleep(1);
+    }
 
 
     return EXIT_SUCCESS;
