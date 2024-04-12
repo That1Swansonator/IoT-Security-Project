41eaa41f296a4c3ca1c7adda7925eb13ef5d1911
patrick <patapoldev@gmail.com>
2024-04-11 19:54:20 -0700

    Reverse engineering the copilot recomendation.

--- RPi5-Loki-Files/test.c
+++ RPi5-Loki-Files/test.c
@@ -6,11 +6,15 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "gpiod.h"
+#include "gpiod.hpp"
+
 
 
 int main(void){
     // select the chip gpiochip0
     struct gpiod_chip *chip = gpiod_chip_open("/dev/gpiochip0");
+
+
 
     if (!chip) {
         perror("Open chip failed\n");
@@ -20,21 +24,13 @@ int main(void){
     // get the line 17
     struct gpiod_line *line = gpiod_chip_get_line(chip, 17);
 
-    if (!line) {
-        perror("Get line failed\n");
-        return EXIT_FAILURE;
-    }
 
     // blink the LED
-    for (int i = 0; i < 10; i++) {
-        gpiod_line_set_value(line, 1);
-        sleep(1);
-        gpiod_line_set_value(line, 0);
-        sleep(1);
-    }
+
 
     // close the line
-    gpiod_line_release(line);
+
+
     //close the chip
     gpiod_chip_close(chip);
 
