dbe4f5baa475abb58bd076f391ca53a06e0c95dd
Patrick Polanco <patapolpersonal@gmail.com>
2024-04-13 21:52:38 -0700

    Bidirrectional data transfer test

--- RPi5-Mobius/AUMobius/AUMobius.ino
+++ RPi5-Mobius/AUMobius/AUMobius.ino
@@ -1,11 +1,11 @@
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-
+    Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("Hello from Arduino!");
-  delay(1000);
+    if (Serial.available() > 0) {
+        String data = Serial.readStringUntil('\n');
+        Serial.print("You sent me: ");
+        Serial.println(data);
+    }
 }
