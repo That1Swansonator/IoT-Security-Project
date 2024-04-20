6a18815f91be346a3fbdfcb07cdce8b43ba728f0
Patrick Polanco <patapolpersonal@gmail.com>
2024-04-19 20:10:22 -0700

    Classing tempSensor.py

--- RPi5-Mobius/tempSensor.py
+++ RPi5-Mobius/tempSensor.py
@@ -2,20 +2,25 @@
 import serial
 import time
 
+class TempSensor:
+    def __init__(self):
+        # Open serial port
+        self.ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+        self.ser.reset_input_buffer()
+
+    def run(self):
+        # Send and receive data
+        while True:
+            line = self.ser.readline().decode('utf8').rstrip()  # read a '\n' terminated line
+
+            try:
+                humidity, temperature = map(float, line.split(','))
+                print(f"Humidity: {humidity}% Temperature: {temperature}°C")
+
+            except ValueError as e:
+                print(f"Error converting data to float: {e}")
+            time.sleep(1)
+
 if __name__ == '__main__':
-    # Open serial port
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-    ser.reset_input_buffer()
-
-    # Send and receive data
-    while True:
-        line = ser.readline().decode('utf8').rstrip()  # read a '\n' terminated line
-        # print(f"Received: {line}")  # print the received data
-
-        try:
-            humidity, temperature = map(float, line.split(','))
-            print(f"Humidity: {humidity}% Temperature: {temperature}°C")
-
-        except ValueError as e:
-            print(f"Error converting data to float: {e}")
-        time.sleep(1)
+    sensor = TempSensor()
+    sensor.run()
