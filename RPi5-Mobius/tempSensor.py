dcc89d2000c3c16478f6bf4b2a670668e860fff1
Patrick Polanco <patapolpersonal@gmail.com>
2024-04-20 12:45:34 -0700

    Added the database insertion method to tempSensor.py

--- RPi5-Mobius/tempSensor.py
+++ RPi5-Mobius/tempSensor.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
+from datetime import datetime
+
 import serial
 import time
+import mysql.connector as mysql
+import os
 
-class TempSensor:
+class tempSensor:
     def __init__(self):
         # Open serial port
         self.ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
@@ -21,6 +25,48 @@ class TempSensor:
                 print(f"Error converting data to float: {e}")
             time.sleep(1)
 
+            # Save data to database
+            # self.save_to_db(temperature)
+    def close(self):
+        self.ser.close()
+
+    def save_to_db(avg_temp):
+        try:
+            # Get password from environment variable
+            password = os.getenv('MY_PASSWORD')
+
+            # get username from environment variable
+            user = os.getenv('MY_USER')
+
+            # get hostname from environment variable
+            host = os.getenv('MY_HOST')
+
+            # get database name from environment variable
+            database = os.getenv('MY_DATABASE')
+
+            # Connect to database
+            db = mysql.connect(
+                host=host, user=user, passwd=password, database=database)
+
+        except Exception as e:
+            print(f"Error connecting to database: {e}")
+
+        # Create a cursor
+        cursor = db.cursor()
+
+        # get the current date and time
+        now = datetime.now()
+        date = now.strftime('%Y-%m-%d')
+        time = now.strftime('%H:%M:%S')
+
+        # Insert data into table
+        cursor.execute(f"insert into TempHistory (tempC, tempDate, tempTime) values (avg_temp, date, time)")
+
+        # Close cursor and database
+        cursor.close()
+        db.close()
+
+
 if __name__ == '__main__':
-    sensor = TempSensor()
+    sensor = tempSensor()
     sensor.run()
