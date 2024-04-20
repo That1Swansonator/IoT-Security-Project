dadecd5764ce36825aa0f4cbff292301d5466ca1
Patrick Polanco <patapolpersonal@gmail.com>
2024-04-19 19:37:51 -0700

    Modifying hvacControls.py in a class, method style.

--- RPi5-Loki-Files/hvacControls.py
+++ RPi5-Loki-Files/hvacControls.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
 import serial
 import time
+import mysql.connector
+
+class HVACControl:
+    def __init__(self):
+        self.ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+        self.ser.reset_input_buffer()
+        self.command = "off"
+
+    def send_receive_data(self):
+        while True:
+            self.command = str(input("Enter command for HVAC (off, ac, hc): "))
+            self.ser.write(self.command.encode()) # Send data over usb
+            line = self.ser.readline().decode('utf-8').rstrip()
+            print(line)
+            time.sleep(1)
 
 if __name__ == '__main__':
-    # Open serial port
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-    ser.reset_input_buffer()
-    command = "off"
-
-    # Send and receive data
-    while True:
-        command = str(input("Enter command for HVAC (off, ac, hc): "))
-        ser.write(command.encode()) # Send data over usb
-        line = ser.readline().decode('utf-8').rstrip()
-        print(line)
-        time.sleep(1)
+    hvac = HVACControl()
+    hvac.send_receive_data()
