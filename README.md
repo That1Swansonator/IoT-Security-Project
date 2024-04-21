3781acc6b9c379948eb91efcf4f96f5139559ad5
Patrick Polanco <patapolpersonal@gmail.com>
2024-04-21 16:10:41 -0700

    tempSensor.py is complete. Starting programming on hvacControls.py orchestrator.py and the installer for linux systems.

--- README.md
+++ README.md
@@ -3,11 +3,5 @@ My primary goal is to create a secure and bespoke IoT network based on the three
 A bash file will; be made as a sort of installer.
 rm /usr/lib/python3.11/EXTERNALLY-MANAGED for removing the PIP installation roadblock
 
-The following bash commands is to install the necessary packages to control the gpio pins on the Raspberry Pi.
-wget https://github.com/joan2937/lg/archive/master.zip
-unzip master.zip
-cd lg-master
-make
-sudo make install
 
 
--- RPi5-Loki-Files/hvacControls.py
+++ RPi5-Loki-Files/hvacControls.py
@@ -1,17 +1,60 @@
 #!/usr/bin/env python3
 import serial
 import time
+import socket
+
+class HVACControls:
+    def __init__(self):
+        self.ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+        self.ser.reset_input_buffer()
+        self.command = "off"
+
+    # set up network socket to recieve command from server
+    def recieveCommand(self):
+        # open a socket to listen for commands
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((socket.gethostname(), 1234))
+        s.listen(5)
+        clientsocket, address = s.accept()
+        print(f"Connection from {address} has been established.")
+        while True:
+            # recieve command from server
+            command = clientsocket.recv(1024)
+
+            if not command:
+                break
+
+            print(command.decode())
+
+            # send command to arduino
+            self.sendCommand(command.decode())
+
+            # send response to server
+            clientsocket.send(bytes("Command recieved", "utf-8"))
+
+
+    def sendCommand(self, command):
+        self.ser.write(command.encode())
+        line = self.ser.readline().decode('utf-8').rstrip()
+        return line
+
+    def closeSerial(self):
+        self.ser.close()
 
 if __name__ == '__main__':
-    # Open serial port
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-    ser.reset_input_buffer()
-    command = "off"
+    hvac = HVACControls()
+    # hvac.sendCommand("ac")
+    hvac.recieveCommand()
 
-    # Send and receive data
-    while True:
-        command = str(input("Enter command for HVAC (off, ac, hc): "))
-        ser.write(command.encode()) # Send data over usb
-        line = ser.readline().decode('utf-8').rstrip()
-        print(line)
-        time.sleep(1)
+    # Open serial port
+    # ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    # ser.reset_input_buffer()
+    # command = "off"
+    #
+    # # Send and receive data
+    # while True:
+    #     command = str(input("Enter command for HVAC (off, ac, hc): "))
+    #     ser.write(command.encode()) # Send data over usb
+    #     line = ser.readline().decode('utf-8').rstrip()
+    #     print(line)
+    #     time.sleep(1)
