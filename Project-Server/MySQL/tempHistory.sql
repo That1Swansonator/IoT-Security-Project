99d2b54cfb69d2b2a020e6828287c77d10370f7b
Patrick Polanco <patapolpersonal@gmail.com>
2024-04-20 19:25:54 -0700

    changed database value to float

--- Project-Server/MySQL/tempHistory.sql
+++ Project-Server/MySQL/tempHistory.sql
@@ -1,6 +1,6 @@
 CREATE TABLE TempHistory(
     tempID INT NOT NULL AUTO_INCREMENT,
-    tempC DECIMAL(5,2) NOT NULL,
+    tempC FLOAT NOT NULL,
     tempDate DATE NOT NULL,
     tempTime TIME NOT NULL,
     PRIMARY KEY (tempID)
