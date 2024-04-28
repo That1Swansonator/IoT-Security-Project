9261d339507f3f9844e61daa5cd3eeb0c1aa7a8e
Patrick Polanco <patapolpersonal@gmail.com>
2024-04-27 20:29:13 -0700

    added method prototypes to the new files.

---
+++ Project-Server/EncryptedComs/communications.py
@@ -0,0 +1,22 @@
+
+class communications:
+    # This class is used to handle all communications between the server and the client
+    # It is used to encrypt and decrypt messages and to manage the OTP distribution
+
+    # Establishes a connection with the client with a pre-shared key
+    def establish_connection():
+        # This function is used to establish a connection with the client
+        # It is used to exchange the public keys and generate the shared secret
+        # It is also used to generate the OTP and send it to the client
+        pass
+
+    def retrieve_keys(self):
+        # This function is used to retrieve the public keys from the
+        pass
+
+    def encrypt_message(message):
+        # This function is used to encrypt a message using the shared secret
+        pass
+
+        
+
