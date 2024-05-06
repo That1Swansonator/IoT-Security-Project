9694a71289803460e53b1631864934919aae1810
Patrick Polanco <patapolpersonal@gmail.com>
2024-05-05 20:11:48 -0700

    communications.py is nearly complete. Working on OTP generation now.

--- Project-Server/EncryptedComs/communications.py
+++ Project-Server/EncryptedComs/communications.py
@@ -4,9 +4,10 @@ from Crypto.Cipher import AES
 import hashlib, secrets, binascii
 import socket
 import threading
-import pyotp
+import os
 
 import ecc_example as ecc
+
 
 # If running as a server, import the hvacControls module
 # import hvacControls
@@ -20,31 +21,49 @@ def get_psk():
         psk = f.read()
     return psk
 
+
 psk = get_psk()
 default_port = 5050
 
+
 # Main Functions
 def main():
-   pass
+    serverside()
 
 
-# Encryption and Decryption Function
-# call ecc_example for these functions
+header = 64
+port = 5050
+FORMAT = 'utf-8'
+disconnect_msg = "!Disconnect"
 
-# Clientside Communications
-def client(d_port):
-    pass
+
+# Clientside Communications. Run this function to connect to a server. Runs once per call
+def clientside(message, d_port=port):
+    # Get a name  from environment variable
+    server = os.getenv('DESTINATION_SERVER')
+    address = (server, d_port)
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(address)
+
+    def send(msg):
+        message = msg.encode(FORMAT)
+        msg_len = len(message)
+
+        send_len = str(msg_len).encode(FORMAT)
+        send_len += b' ' * (header - len(send_len))
+
+        client.send(send_len)
+        client.send(message)
+        print(client.recv(2048).decode())
+
+    send(message)
 
 
 # Serverside communications
-# d_port is the port that the server will listen on. Can be called recursively.
-def server(d_port):
-    header= 64
-    port = d_port
+def serverside(d_port=port, end=False):
     server = socket.gethostbyname(socket.gethostname())
-    address = (server, port)
-    FORMAT = 'utf-8'
-    disconnect_msg = "!Disconnect"
+    address = (server, d_port)
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(address)
@@ -64,28 +83,16 @@ def server(d_port):
                     connected = False
 
                 print(f"[{address}] {msg}")
-                conn.send("Msg Recieved".encode(FORMAT))
+                conn.send("!RECIEVED".encode(FORMAT))
+
+                if end:
+                    #kill the server
+                    server.close()
+                    msg_interpreter(msg)
+
+                msg_interpreter(msg)
 
         conn.close()
-
-    def msg_interpreter(msg):
-        msg = decrypt_AES_GCM(msg, psk)
-        # slip the message into a command and an argument on the : character
-        cmd, arg = msg.split(":")
-
-        if cmd == "!START":
-            print(f"Starting Server at port {arg}")
-            server(arg)
-
-        if cmd == "!TEMP":
-            pass
-
-        if cmd == "!END":
-            print("Ending Server")
-            server.close()
-
-        else:
-            print("Invalid Command")
 
     def start():
         server.listen()
@@ -95,14 +102,14 @@ def server(d_port):
             conn, address = server.accept()
             thread = threading.Thread(target=handle_client, args=(conn, address))
             thread.start()
-            print(f"[ACTIVE CONNECTIONS] {threading.active_count()-1}")
+            print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
 
     print("[STARTING] server is starting")
     start()
 
+
 def client():
-    header= 64
-    port = 5050
+    header = 64
     FORMAT = 'utf-8'
     disconnect_msg = "!Disconnect"
     server = "192.168.1.59"
@@ -121,6 +128,30 @@ def client():
         client.send(send_len)
         client.send(message)
         print(client.recv(2048).decode())
+
+
+def msg_interpreter(msg):
+    msg = ecc.decrypt_ECC(msg, psk)
+
+    # split the message into a command and an argument on the : character. Might add more splitting later
+    cmd, arg = msg.split(":")
+
+    # Start the key exchange process. The argument is the public key of the incoming client
+    if cmd == "!KEP":
+        clientside()
+
+    if cmd == "!NE":
+        # Create a new instance of the server at a different port if needed and
+        # kills the instance after running
+        serverside(arg, True)
+
+
+    if cmd == "!TEMP":
+        pass
+
+    else:
+        print("Invalid Command")
+
 
 if __name__ == '__main__':
     main()
